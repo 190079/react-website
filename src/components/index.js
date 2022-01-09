774fa341828086212854c97dd708c74df9ab89f7
190079 <190079@softwarica.edu.np>
2022-01-09 20:38:29 +0545

    second commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { default as Article } from "./article/Article";
+import { default as Brand } from "./brand/Brand";
+import { default as CTA } from "./cta/CTA";
+import { default as Feature } from "./feature/Feature";
+import { default as NavBar } from "./navbar/NavBar";
+
